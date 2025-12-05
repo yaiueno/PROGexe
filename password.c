c701a7af62677af13b3843cbc4f396d9336ebe23
yokun <101389513+yaiueno@users.noreply.github.com>
2025-12-05 18:58:27 +0900

    Add files via upload
    not perfect

---
+++ password.c
@@ -0,0 +1,22 @@
+//未完成まだ採点しないでください
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct passchar{
+    char pass;
+    struct.passchar *next;
+    } passchar;
+
+void add_pass_to_front(int v){
+
+}
+
+int main(){
+    char word;
+    int counter=0;
+    while(scanf(%c, word) =! EOF){
+        counter++ ;
+
+    }
+}
